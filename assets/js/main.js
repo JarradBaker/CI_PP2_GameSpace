e9c4577c9f845c8a912766bf3f12f2ee5ad05e48
JarradBaker <jarradbaker@outlook.com>
2023-01-23 22:05:22 +0000

    Add: functionality for hamburger menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,5 +2,10 @@ const hamburger = document.getElementsByClassName('hamburger')[0];
 const navLinks = document.getElementsByClassName('nav-links')[0];
 
 hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('hidden');
+    if(navLinks.getAttribute('id', 'hide')){
+        navLinks.removeAttribute('id', 'hide')
+    } else {
+        navLinks.setAttribute('id', 'hide');
+    }
+    
 } );
